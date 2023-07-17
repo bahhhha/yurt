bae730a2c9661fa080ec8a544b8e2cde47eb1401
bph21 <48647702+mintjulep21@users.noreply.github.com>
2023-07-18 02:36:58 +0600

    Delete front directory

--- front/app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-	return (
-		<footer className=" text-[#304d72] text-center pb-2 bottom-0">
-			&copy; YurtAI {new Date().getFullYear()}
-		</footer>
-	);
-};
-
-export default Footer;
